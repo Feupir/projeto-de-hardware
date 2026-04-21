adac82e192e596729e5c42f10d3907e86c680631
Felipe Santos <Felipe.gabsantos25@gmail.com>
2026-04-21 20:56:56 -0300

    Tem que trocar os X para os valores certos

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,39 +1,42 @@
-// Felipe : 11/04 
+// Felipe : 11/04
 // Projeto de Hardware da UEPG *Trabalhando como calouro pros parceiro do quinto ano;
 // Tem comentário em tudo pra eu lembrar o que fazer, tenho TDAH :)
-// Atualização só para as correções que o James comentou, continuarei escrevendo o codigo na segunda a noite.
 
 #include <PubSubClient.h>
 #include <WiFi.h>
-#define PortaBotao 16
-  
+#include <SparkFun_APDS9960.h>
+#include <Wire.h>
+
+
+#define PinoAPDS 2
+
 const char* ssid = "NOMEDAREDE";
 const char* senha = "SenhaDaRede";
 const char* mqtt_server "Insira o IP aqui";
 const int mqtt_port = 1883;
-const char* topicoUnico = "bacias/comando/proxima"
-int botaoPress = 0;
+const char* topicoUnico = "bacias/comando/proxima" ;
+sensorGesto apds = SparkFun_APDS9960();
+int isr_flag = 0; // Variavel da interrupção do sensor de gesto.
 
-void conectarWiFi(){
-  WiFi.begin(ssid,senha); // Tenta conctar
+void conectarWiFi() {
+  WiFi.begin(ssid, senha);  // Tenta conctar
 
-  while (WiFi.status() != WL_CONNECTED()){ // Enquanto não ta conectado
+  while (WiFi.status() != WL_CONNECTED()) {  // Enquanto não ta conectado
     delay(500);
     Serial.println("To conectano, calma, eu trabalho pior sobre pressão");
   }
   Serial.println("Conectei !!! :D");
   Serial.println(WiFi_localIP();)
 }
-void mqttReconect(){
-  while (!client.connected()){
+void mqttReconect() {
+  while (!client.connected()) {
     Serial.println("Conectando o MQTT");
     String clientId = "ENGEASIER_MQTT";
     clientId += String(random(0xffff), HEX);
-    if (client.connect(clientID.c_str())){
+    if (client.connect(clientID.c_str())) {
       Serial.println("Conectado ( eu acho )")
-      client.subscribe(topicoUnico);
-    }
-    else {
+        client.subscribe(topicoUnico);
+    } else {
       Serial.print("Falha ao conectar = ");
       Serial.print(client.state());
       Serial.println("Tentando reconecção em 5 segundos");
@@ -42,30 +45,57 @@ void mqttReconect(){
   }
 }
 
+void handleGesture() {
+    if ( apds.isGestureAvailable() ) {
+    switch ( apds.readGesture() ) {
+      case DIR_UP:
+        client.publish(topico, X) // X para o valor de abrir detalhes técnicos.
+        break;
+      case DIR_DOWN:
+        client.publish(topico, X) // X para o valor de voltar para o inicio.
+        break;
+      case DIR_LEFT:
+        client.publish(topico, X) // X para o valor de voltar uma Bacia.
+        break;
+      case DIR_RIGHT:
+        client.publish(topico, X) // X para o valor de ir pra próxima bacia.
+        break;
+      default:
+        Serial.println("NONE");
+    }
+  }
+}
+
+void interruptRoutine(){
+  isr_flag =1;
+}
+
+
 void setup() {
   Serial.begin(115200);
   conectarWiFi();
-  
+  mqttReconect();
+  pinMode(PinoAPDS, INPUT);
+  attachInterrupt(0, interruptRoutine, FALLING); // Interrupção pro sensor só funcionar quando algum gesto estiver sendo feito.
 
-  pinMode(PortaBotao, INPUT_PULLUP); // Declara a porta do botão
 }
 
 void loop() {
-  //Reconectar o Wifi se tiver dado merda 
-  if (WiFi.status() != WL_CONNECTED){
-    WiFi.begin(ssid,senha);  // Não sei se é assim que reconecta XD pesquisa depois
-    while (WiFi.status() != WL_CONNECTED){ // Enquanto não ta conectado
+  //Reconectar o Wifi se tiver dado merda
+  if (WiFi.status() != WL_CONNECTED) {
+    WiFi.begin(ssid, senha);                 // Não sei se é assim que reconecta XD pesquisa depois
+    while (WiFi.status() != WL_CONNECTED) {  // Enquanto não ta conectado
       delay(500);
       Serial.println("To conectano, calma, eu trabalho pior sobre pressão");
     }
     Serial.println("Conectei !!! :D");
   }
-  //ler o botão
-  botaoPress = digitalRead(PortaBotao);
-
-  if (botaoPress == LOW){
-    client.publich(topico, 1);
-    delay(200); //Debouce improvisado por enquanto
+  mqttReconect();
+  //Sensor de gesto a partir daqui
+  if( isr_flag == 1 ) {
+    detachInterrupt(0);
+    handleGesture();
+    isr_flag = 0;
+    attachInterrupt(0, interruptRoutine, FALLING);
   }
-  //Conhectar o MQTT
 }
